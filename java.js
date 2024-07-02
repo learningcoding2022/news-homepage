7d2193b8367c02ff49f15f604414e21aa8ebe7a1
Molly <117697419+learningcoding2022@users.noreply.github.com>
2024-07-02 14:27:34 -0400

    changes to java?

--- java.js
+++ java.js
@@ -9,7 +9,7 @@ window.onload = function () {
 
     const menu_btn = document.querySelector('.hamburger');
 
-    menu_btn.addEventListener('click', function() {
+    menu_btn.addEventListener('click', function () {
         menu_btn.classList.toggle('is-active');
     });
 
